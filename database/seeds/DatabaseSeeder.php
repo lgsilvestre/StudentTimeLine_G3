340fe56a354ae44c32aa584814699cf8f6989ef1
Isavo Castro <isavocastro@gmail.com>
2020-05-19 21:21:32 -0400

    @Yngwiie Creación de datos semilla
    Creación de datos semilla de la base de datos.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        //$this->call(PermissionsTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        
+
     }
 }
