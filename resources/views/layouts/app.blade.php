20543d2bb25623ab8603267b729c701d30c31524
Rubén Ramírez <rubenrv95@hotmail.com>
2020-05-27 00:17:38 -0400

    @ArielVF Vista de usuario barra de navegación
    Se avanzó en la barra de navegación para que esta se muestre después de logearse exitosamente.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,8 +47,11 @@
                                 </li>
                             @endif
                         @else
+                        <li class="nav-item "> <a class="nav-link active" href="" style="color:#ffff" >{{ __('Inicio') }}</a> </li>
+                        <li class="nav-item "> <a class="nav-link active" href="" style="color:#ffff">{{ __('Roles') }}</a> </li>
+                        <li class="nav-item "> <a class="nav-link active" href="" style="color:#ffff">{{ __('Usuarios') }}</a> </li>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="color:#ffff">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
