8cb35b926d173a674e1478697dc428c798ccbf06
Rubén Ramírez <rubenrv95@hotmail.com>
2020-06-03 16:34:59 -0400

    @ArielVF Vista Editar usuario
    Se creó la vista de editar usuario. Aún falta por añadir todas sus funcionalidades y hay un error con el logo de la universidad.

---
+++ resources/views/Usuario/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Editar</div>
+
+                <div class="card-body">                   
+                   Vista de edición. Aquí se editara el usuario
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
