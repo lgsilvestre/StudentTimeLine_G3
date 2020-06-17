472b2fc462a3e06d692e274b0578d3c2386e097f
Ignacio Burgos <iburgos16@alumnos.utalca.cl>
2020-06-17 16:09:56 -0400

    se agrega las imagenes de carrera
    se agregan a los seeders y a la migración el atributo imagen para subir la foto de la carrera correspondiente.

--- database/seeds/CarreraTableSeeder.php
+++ database/seeds/CarreraTableSeeder.php
@@ -15,30 +15,37 @@ class CarreraTableSeeder extends Seeder
         Carrera::create([
             'nombre' => 'Ingeniería Civil en Computación',
             'codigo_carrera' => 3407,
+            'imagen' => 'icc.png',
         ]);
         Carrera::create([
             'nombre' => 'Ingeniería Civil Industrial',
             'codigo_carrera' => 3406,
+            'imagen' => 'ici.png',
         ]);
         Carrera::create([
             'nombre' => 'Ingeniería Civil en Obras Civiles',
             'codigo_carrera' => 3408,
+            'imagen' => 'icoc.png',
         ]);
         Carrera::create([
             'nombre' => 'Ingeniería Civil de Minas',
             'codigo_carrera' => 3409,
+            'imagen' => 'icm.png',
         ]);
         Carrera::create([
             'nombre' => 'Ingeniería Civil Mecatrónica',
             'codigo_carrera' => 3404,
+            'imagen' => 'meca.png',
         ]);
         Carrera::create([
             'nombre' => 'Ingeniería Civil Mecánica',
             'codigo_carrera' => 3405,
+            'imagen' => 'icm.png',
         ]);
         Carrera::create([
             'nombre' => 'Ingeniería Civil Eléctrica',
             'codigo_carrera' => 3410,
+            'imagen' => 'ice.png',
         ]);
     }
 }
