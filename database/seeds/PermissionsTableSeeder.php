dbe954619dd1ea0f6e712389671c8fb7094b466d
Javier Aliaga <reivaj_31@hotmail.com>
2020-07-01 16:11:47 -0300

    @is4vo Creación de seeder de permisos
    Se crean seeder de los permisos que se le asignan a los roles para poder ver ciertas partes de la aplicación con los permisos.

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -28,7 +28,65 @@ class PermissionsTableSeeder extends Seeder
             'slug' => 'modulos.index',
             'description' => 'Lista y navega todos los modulos del sistema',
         ]);
+        Permission::create([
+            'name' => 'Añadir estudiante',
+            'slug' => 'estudiante.add',
+            'description' => 'Podra añadir estudiante de la carrera que se le asigne',
+        ]);
 
+        /** 
+         * 
+         * Añadir usuarios por carrera
+         * 
+         * 
+        */
+        
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil en computación',
+            'slug' => 'computacion.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil en computación',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil industrial',
+            'slug' => 'industrial.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil industrial',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil mecanica',
+            'slug' => 'mecanica.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil mecanica',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil electrica',
+            'slug' => 'electrica.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil electrica',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil en obras civiles',
+            'slug' => 'obras.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil en obras civiles',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil en minas',
+            'slug' => 'minas.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil en minas',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil mecatronica',
+            'slug' => 'mecatronica.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil mecatronica',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil en Bioinformatica',
+            'slug' => 'bioinformatica.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil en Bioinformatica',
+        ]);
+        Permission::create([
+            'name' => 'Añadir usuario carrera ing civil en Desarrollo de Videojuegos y Realidad Virtual',
+            'slug' => 'videojuegos.addUser',
+            'description' => 'Podra añadir usuario de la carrera ing civil en Desarrollo de Videojuegos y Realidad Virtual',
+        ]);
+        
         
     }
 }
