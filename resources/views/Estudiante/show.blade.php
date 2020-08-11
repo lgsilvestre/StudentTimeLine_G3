d8bb1d2b56009c690e472ad457ff6f56bde68bff
Javier Aliaga <reivaj_31@hotmail.com>
2020-08-11 13:48:04 -0300

    Arreglando detalles en la importacion de estudiantes
    Se les da formato a las columnas de rut y sit actual.

--- resources/views/Estudiante/show.blade.php
+++ resources/views/Estudiante/show.blade.php
@@ -321,7 +321,7 @@
 
 <!--Modal WIZARD editar ESTUDIANTE-->
 <div id="modal_editar_wizard" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-xl">
     <div class="modal-content">
 
       <div class="modal-header custom-header custom-color">
