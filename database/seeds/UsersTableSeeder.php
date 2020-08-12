60581d46689b887fb273f996e840a24924435786
Javier Aliaga <reivaj_31@hotmail.com>
2020-08-12 15:39:40 -0300

    Crear cuentas administradores para presentación
    Cuentas de administradores para presentación final, y ademas se arregla un modal de crear carreras.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,21 @@ class UsersTableSeeder extends Seeder
         ]);
         $user->assignRoles('admin');
         $user->save();
-        
+        $user = User::create([
+            'name' => 'Admin1',
+            'email_verified_at' => now(),
+            'email' => 'javieracabrera14@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $user->assignRoles('admin');
+        $user->save();
+        $user = User::create([
+            'name' => 'Admin2',
+            'email_verified_at' => now(),
+            'email' => 'iburgos16@alumnos.utalca.cl',
+            'password' => bcrypt('12345678'),
+        ]);
+        $user->assignRoles('admin');
+        $user->save();
     }
 }
